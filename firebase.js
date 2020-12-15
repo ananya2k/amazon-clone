7d46abf28abe0b6d6507c7812bccdea76f4d9da3
ananya2k <ananya2k@gmail.com>
2020-12-15 14:05:25 -0500

    first commit

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBTRgfvSYA3NJ9DRCYwWBHTfF-z0PiO-s0",
+  authDomain: "clone-8c3bf.firebaseapp.com",
+  projectId: "clone-8c3bf",
+  storageBucket: "clone-8c3bf.appspot.com",
+  messagingSenderId: "19067259238",
+  appId: "1:19067259238:web:5ee0ab023cc3bf1199a0a7",
+  measurementId: "G-ZN5B31WTVM"
+};
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    // BEM
+    <div className="App">
+     <h1> Amazon clone coming up!!!!!! </h1>
+    </div>
+  );
+}
+
+export default App;
